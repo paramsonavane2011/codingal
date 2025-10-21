4bf4422fa367fd249621079e3195d6cdc527f2d9
paramsonavane2011 <136975049+paramsonavane2011@users.noreply.github.com>
2025-10-21 18:36:03 +0530

    Add files via upload

---
+++ day5activity3.py
@@ -0,0 +1,8 @@
+num = 15
+i = int(input("Enter a number to check if it is greater than 15 or not: "))
+if i > num:
+    print("The number is greater than 15.")
+    print("Condition is in \"if\" block.")
+else:
+    print("The number is not greater than 15.")
+    print("Condition is in \"else\" block.")
