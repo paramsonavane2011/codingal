87c9ebd2ac828f80f5373b67833d11300ef9076a
paramsonavane2011 <136975049+paramsonavane2011@users.noreply.github.com>
2025-12-11 18:10:56 +0530

    Add files via upload

---
+++ day20activity1.py
@@ -0,0 +1,11 @@
+import datetime
+
+present = datetime.datetime.now()
+
+date = datetime.date.today()
+
+print(present)
+print(date)
+print(f"Year: {date.year}")
+print(f"Month: {date.month}")
+print(f"Day: {date.day}")
