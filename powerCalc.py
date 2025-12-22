f7ca82b5d96f60bea7ef8e0b64a6afb2c155d209
paramsonavane2011 <136975049+paramsonavane2011@users.noreply.github.com>
2025-12-22 21:03:46 +0530

    Add files via upload

---
+++ powerCalc.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+power = int(input("Enter the power to raise the number to: "))
+
+result = num ** power
+
+print(f"{num} raised to the power of {power} is {result}")
