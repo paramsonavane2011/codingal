e8cac611ece85a7732eeadef09823a999c87b54a
paramsonavane2011 <136975049+paramsonavane2011@users.noreply.github.com>
2025-12-22 21:20:56 +0530

    Add files via upload

---
+++ change.py
@@ -0,0 +1,4 @@
+bill = int(input("Enter the total bill amount: "))
+given = int(input("Enter the amount given by the customer: "))
+change = given - bill
+print("The change to be returned is:", change)
