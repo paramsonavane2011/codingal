028b005e66e7b15f93ae02f6b0f89c402a262e36
paramsonavane2011 <136975049+paramsonavane2011@users.noreply.github.com>
2025-12-22 20:56:24 +0530

    Create swapNumbers.py

---
+++ swapNumbers.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+num1, num2, num3 = num3, num1, num2
+
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+print("Third number:", num3)
