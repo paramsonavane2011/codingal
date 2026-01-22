08ae5fa165c04fe7482a140c07ca7d97c4bc3d04
paramsonavane2011 <136975049+paramsonavane2011@users.noreply.github.com>
2026-01-22 18:34:08 +0530

    Replace Class with Computer class implementation

--- day30activity2.py
+++ day30activity2.py
@@ -1,10 +1,14 @@
-class Class():
-    __privVar = 123
-    def __privFunc(self):
-        print("hi")
-    def hello(self):
-        print(self.__privVar)
-
-object = Class()
-object.hello()
-object.__privFunc()
+class Computer():
+    def __init__(self):
+        self.__maxPrice = 69
+    def price(self):
+        print(self.__maxPrice)
+    def setMaxPrice(self, price):
+        self.__maxPrice = price
+    
+com = Computer()
+com.price()
+com.setMaxPrice(70)
+com.price()
+com.__maxPrice = 71
+com.price()
