b4c5d50c5b3cee6f007f2a967d9120c2e0e65415
paramsonavane2011 <136975049+paramsonavane2011@users.noreply.github.com>
2026-02-11 15:38:56 +0530

    Add files via upload

---
+++ symDiff.py
@@ -0,0 +1,11 @@
+set1 = {"blue", "green"}
+set2 = {"blue", "yellow"}
+
+sym_diff = set1.symmetric_difference(set2)
+print(sym_diff)
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 5, 6, 7, 8, 9}
+
+sym_diff = set1.symmetric_difference(set2)
+print(sym_diff)
